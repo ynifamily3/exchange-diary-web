e82c87e8e5ae432472e1006a60a0f8fbe471ec4c
Jongkeun Choi <jongkeun.ch@gmail.com>
2022-01-24 00:05:30 +0900

    rev

--- middleware/index.ts
+++ middleware/index.ts
@@ -1,14 +1,16 @@
-const Aop = {
-  around: function (fnName, advice, fnObj) {
-    let originalFn = fnObj[fnName];
-    fnObj[fnName] = function (...arguments) {
-      return advice.call(this, {
-        fn: originalFn,
-        args: arguments,
-      });
-    };
-  },
-  next: function (targetInfo) {
-    return targetInfo.fn.apply(this, targetInfo.args);
-  },
-};
+// const Aop = {
+//   around: function (fnName, advice, fnObj) {
+//     let originalFn = fnObj[fnName];
+//     fnObj[fnName] = function (...arguments) {
+//       return advice.call(this, {
+//         fn: originalFn,
+//         args: arguments,
+//       });
+//     };
+//   },
+//   next: function (targetInfo) {
+//     return targetInfo.fn.apply(this, targetInfo.args);
+//   },
+// };
+
+export {};
